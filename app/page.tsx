541b1d99b89511cdcf57f264bb83a7bdc2dfb407
Nevzat Atalay <nevzatatalay79@gmail.com>
2025-01-12 10:24:38 +0300

    update minor change

---
+++ app/page.tsx
@@ -0,0 +1,20 @@
+
+import Encryption from "@/components/main/Encryption";
+import Hero from "@/components/main/Hero";
+import Projects from "@/components/main/Projects";
+import Skills from "@/components/main/Skills";
+import Theme from "./Theme";
+
+export default function Home() {
+  return (
+    <main className="h-full w-full">
+      <div className="flex flex-col gap-20">
+        <Hero />
+        <Skills />
+        <Encryption />
+        <Projects />
+        <Theme />
+      </div>
+    </main>
+  );
+}
